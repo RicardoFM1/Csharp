79fe9c269454e1c8551090ea09278076fd4cb9b1
RICARDO FERNANDES DE MOURA <05380295010@senacrs.edu.br>
2025-04-07 19:21:50 -0300

    Aulas Csharp

---
+++ Aula 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int multiplosde5 = 100;
+            
+            while (multiplosde5 >= 0)
+            {
+                Console.WriteLine(multiplosde5);
+                multiplosde5 = multiplosde5 - 5;
+            }
+        }
+    }
+}
