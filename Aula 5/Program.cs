a0f475654c2116489fb69a9e5bf16cb840f90635
RICARDO FERNANDES DE MOURA <05380295010@senacrs.edu.br>
2025-04-07 19:28:11 -0300

    Aulas Csharp

--- Aula 5/Program.cs
+++ Aula 5/Program.cs
@@ -1,18 +1,89 @@
-﻿using System;
+﻿// ATIVIDADE 1:
 
-namespace Aula5
+//using System;
+
+//namespace atividades
+//{
+//    class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int multiplosde5 = 100;
+//            while (multiplosde5 >= 0)
+//            {
+//                Console.WriteLine(multiplosde5);
+//                multiplosde5 = multiplosde5 - 5;
+//            }
+//        }
+//    }
+//}
+
+
+// ATIVIDADE 2:
+
+using System;
+
+namespace atividades
 {
     class program
     {
         static void Main(string[] args)
         {
-            int multiplosde5 = 100;
-            
-            while (multiplosde5 >= 0)
+            Console.WriteLine("Digite um número inteiro:");
+            if (int.TryParse(Console.ReadLine(), out int numero) && numero > 0)
             {
-                Console.WriteLine(multiplosde5);
-                multiplosde5 = multiplosde5 - 5;
+
+                int somaPares = 0;
+                long ProdutoImpares = 1;
+                bool temImpar = false;
+                int i = 1;
+
+                Console.WriteLine("Contando de 1 até " + numero + ":");
+                while (i <= numero)
+                {
+                    Console.WriteLine(i);
+
+                    if (i % 2 == 0)
+                    {
+                        somaPares += i;
+                    }
+                    else
+                    {
+                        ProdutoImpares *= i;
+                        temImpar = true;
+                    }
+                    i++;
+
+                }
+                Console.WriteLine("\nA soma dos pares no intervalo do seu número é: " + somaPares);
+                Console.WriteLine("Produto dos números ímpares no intervalo: " + (temImpar ? ProdutoImpares : 0));
+
+            }
+            else
+            {
+                Console.WriteLine("Insira um valor inteiro positivo válido.");
             }
         }
+
     }
 }
+//using System;
+
+//namespace atividades
+//{
+//    class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int numero = 0;
+//            while (numero <= 10)
+//            {
+//                Console.WriteLine(numero);
+//                numero++;
+                
+               
+//            }
+//        }
+//    }
+//}
+
